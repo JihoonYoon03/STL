876179803f65a50782f467ee8829c17c0a05d022
laptop-msi <wlgns7922@gmail.com>
2026-03-04 12:26:16 +0900

    Add: 소스코드 save 기능 완성

--- main.cpp
+++ main.cpp
@@ -1,40 +1,14 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				3월 3일					(1주 1일)
+// 2026 1학기 STL				3월 4일					(1주 2일)
 //------------------------------------------------------------------
-// VS 버전		- 17.14.27
-// 컴파일 환경	- Release / x64
-// 프로젝트 설정 - 일반 - C++ 언어 표준 - /std::c++latest
-//				- C/C++ - SDL 설정 - 아니요
+// template
 //------------------------------------------------------------------
 
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <algorithm>
 
-void save( const std::string& );
+#include "save.h"
 
 int main()
 {
-	std::cout << "2026년 3월 3일" << '\n';
-
 	save( "main.cpp" );		// "메인.cpp"를 저장하자
-}
-
-void save( const std::string& fname )
-{
-	std::cout << '\"' << fname << "\" 저장합니다" << '\n';
-
-	std::ifstream in { fname };		// RAII - Resource Acquisition Is Initialization
-	if ( not in )
-	{
-		std::cout << fname << " - 열 수 없습니다" << '\n';
-		exit(20260303);
-	}
-
-	std::ofstream out { "2026 1학기 STL.txt", std::ios::app };
-
-	// in의 내용을 읽어서 out에 덧붙이자.
-	std::copy(std::istreambuf_iterator<char>{ in }, {},
-		std::ostream_iterator<char>{ out });
 }
---
+++ save.cpp
@@ -0,0 +1,43 @@
+//------------------------------------------------------------------
+// 한 학기 강의를 저장하는 함수
+// 
+// 2026. 3. 4												jihoon
+//------------------------------------------------------------------
+
+#include <chrono>
+#include <iostream>
+#include <fstream>
+
+#include "save.h"
+
+void save(const std::string& fname)
+{
+	std::cout << '\"' << fname << "\" 저장합니다" << '\n';
+
+	std::ifstream in{ fname };		// RAII - Resource Acquisition Is Initialization
+	if (not in)
+	{
+		std::cout << fname << " - 열 수 없습니다" << '\n';
+		exit(20260304);
+	}
+
+	std::ofstream out{ "2026 1학기 STL.txt", std::ios::app };
+
+	// 저장한 시간을 out에 기록하자
+	// std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	// 시간을 한글로 저장하자
+	auto now = std::chrono::system_clock::now();
+	using namespace std::chrono_literals;
+
+	out << "====================================================================" << '\n';
+	out << "저장 시간 - " << now + 9h << '\n';
+	out << "====================================================================" << '\n';
+	out << '\n';
+
+	// in의 내용을 읽어서 out에 덧붙이자.
+	// std::copy(원본, 복사본)
+	std::copy(std::istreambuf_iterator<char>{ in }, {},
+		std::ostream_iterator<char>{ out });
+
+	out << '\n\n';
+}

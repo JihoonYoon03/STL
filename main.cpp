6a7391ba14937e827533334ae75964db321754e8
laptop-msi <wlgns7922@gmail.com>
2026-03-10 15:19:52 +0900

    template 및 랜덤값

--- main.cpp
+++ main.cpp
@@ -1,48 +1,32 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				3월 4일					(1주 2일)
+// 2026 1학기 STL				3월 10일					(2주 1일)
 //------------------------------------------------------------------
-// template
+// 많은 수의 자료를 처리하기
 //------------------------------------------------------------------
 
 #include <iostream>
+#include <random>
+#include <print>
 
 #include "save.h"
 
-class Dog;				// forward declaration
-
-template <class T>
-void change(T& a, T& b)
-{
-	T temp{ a };		// copy constructor
-	a = b;				// a.operator= (b);
-	b = temp;
-}
-
-class Dog {
-public:
-	Dog() = default;
-	Dog(int input) : value{ input } {}
-
-private:
-	int value{};
-
-	friend std::ostream& operator<<(std::ostream& os, const Dog& dog) {
-		return os << dog.value;
-	}
-};
+std::default_random_engine dre;
+std::uniform_int_distribution<int> uid{ 0, 9999 };
 
 int main()
 {
+	// [문제] 임의의 값을 갖는 int 1000개를 생성하라. 모든 값을 화면에 출력하라.
+
+	int num[1000];
+
+	for (int i = 0; i < 1000; ++i)
 	{
-		int a{ 1 }, b{ 2 };
-		change<int>(a, b);
-		std::cout << a << ", " << b << std::endl;		//  2, 1
+		num[i] = uid(dre);
 	}
 
+	for (int n : num)
 	{
-		Dog a{ 1 }, b{ 2 };
-		change<Dog>(a, b);
-		std::cout << a << ", " << b << std::endl;		//  2, 1
+		std::print("{:8}", n);
 	}
 
 	save( "main.cpp" );		// "메인.cpp"를 저장하자

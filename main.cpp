03abc9f82597c021db80a75e3f9e953b512a8e9d
laptop-msi <wlgns7922@gmail.com>
2026-03-11 13:21:16 +0900

    많은 데이터 처리

--- main.cpp
+++ main.cpp
@@ -1,33 +1,39 @@
-//------------------------------------------------------------------
-// 2026 1학기 STL				3월 10일					(2주 1일)
+//--------------------------------- ---------------------------------
+// 2026 1학기 STL				3월 11일					(2주 2일)
 //------------------------------------------------------------------
 // 많은 수의 자료를 처리하기
 //------------------------------------------------------------------
 
 #include <iostream>
 #include <random>
-#include <print>
+#include <fstream>
+#include <array>
 
 #include "save.h"
 
-std::default_random_engine dre;
-std::uniform_int_distribution<int> uid{ 0, 9999 };
+std::default_random_engine dre{ };
+std::uniform_int_distribution<int> uid{ 0, 999'9999 };
 
 int main()
 {
-	// [문제] 임의의 값을 갖는 int 1000개를 생성하라. 모든 값을 화면에 출력하라.
+	// [문제] "int값1000만개.txt"에 저장한 int값을 읽어 화면에 저장하라.
+	/*std::ofstream out { "int값1000만개.txt" };
 
-	int num[1000];
+	for (int i = 0; i < 1000'0000; ++i) {
+		out << uid(dre) << ' ';
+	}*/
 
-	for (int i = 0; i < 1000; ++i)
-	{
-		num[i] = uid(dre);
+	std::ifstream in{ "int값1000만개.txt" };
+
+	if (not in) {
+		std::cout << "파일 읽기 실패" << std::endl;
+		return 2022184025;
 	}
 
-	for (int n : num)
-	{
-		std::print("{:8}", n);
+	int num = 0;
+	while (in >> num) {
+		std::cout << num << std::endl;
 	}
 
-	save( "main.cpp" );		// "메인.cpp"를 저장하자
+	//save( "main.cpp" );		// "메인.cpp"를 저장하자
 }

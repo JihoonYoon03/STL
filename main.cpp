973f4b206a566ba2c0c4a84a16b34fd2a7c47e1d
E219-34 <E219-34@DESKTOP-ICRCJ2H>
2026-03-18 13:19:16 +0900

    Add: string이 포함된 클래스의 바이너리 저장시 문제점

--- main.cpp
+++ main.cpp
@@ -1,35 +1,51 @@
 //-------------------------------------------------------------------
-// 2026 1학기 STL				3월 17일					(3주 1일)
+// 2026 1학기 STL				3월 18일					(3주 2일)
 //------------------------------------------------------------------
 // 많은 수의 자료를 처리하기 - binary I/O
 //------------------------------------------------------------------
 
 #include <iostream>
-#include <fstream>
-#include <array>
 #include <print>
+#include <string>
+#include <array>
+#include <fstream>
 
 #include "save.h"
 
-int main()
+class Dog {
+public:
+	Dog( ) {}
+private:
+	std::string name = "";		// 10 이상 30 이하 임의의 소문자
+	int num = 0;
+
+	friend std::ostream& operator<<(std::ostream& os , const Dog& dog) {
+		std::print(os , "[{:4}] - {}" , dog.num , dog.name);
+		return os;
+	}
+};
+
+int main( )
 {
-	// [문제] 파일 "int 천개.bin"에는 1000개의 int값이 저장되어있다.
-	// 파일은 binary mode
-	// 메모리에 int 1000개 값을 읽어 오세요.
+	// [문제] 바이너리 모드로 저장한 파일 "Dog백마리"에는 Dog 객체 100개가 저장되어 있다.
+	// Dog 객체는 파일의 write 함수를 사용하여 객체 크기 그대로 저장하였다.
 
-	std::ifstream in{ "int 천개.bin", std::ios::binary };
+	// 파일을 읽어 array에 저장하라.
+	// 객체 수 100개는 실행동안 변경되지 않는다.
 
-	if (not in) {
-		std::cout << "잘못된 파일입니다" << std::endl;
+	std::ifstream in{ "Dog백마리", std::ios::binary };
+
+	if ( not in ) {
+		std::cout << "파일을 열 수 없습니다" << std::endl;
+		return 2022184025;
 	}
 
-	std::array<int, 1000> a;
+	std::array<Dog , 100> dogs;
+	in.read(reinterpret_cast< char* >( dogs.data( ) ) , sizeof(Dog) * dogs.size( ));
 
-	in.read(reinterpret_cast<char*>(a.data()), a.size() * sizeof(int));
-	
-	for (int num : a) {
-		std::print("{:8}", num);
+	for ( const Dog& dog : dogs ) {
+		std::cout << dog << std::endl;
 	}
 
-	save( "main.cpp" );		// "메인.cpp"를 저장하자
+	save("main.cpp");		// "메인.cpp"를 저장하자
 }

d5e80816a16710a81e9ca35a914395543ed646d6
laptop-msi <wlgns7922@gmail.com>
2026-03-24 15:18:38 +0900

    Add: 스마트 포인터, numeric 함수

--- main.cpp
+++ main.cpp
@@ -1,51 +1,39 @@
-//-------------------------------------------------------------------
-// 2026 1학기 STL				3월 18일					(3주 2일)
 //------------------------------------------------------------------
-// 많은 수의 자료를 처리하기 - binary I/O
+// 2026 1학기 STL				3월 24일					(4주 1일)
+//------------------------------------------------------------------
+// smart pointer - callable type
 //------------------------------------------------------------------
 
 #include <iostream>
-#include <print>
-#include <string>
-#include <array>
-#include <fstream>
+#include <numeric>
+#include <memory>
 
 #include "save.h"
 
-class Dog {
-public:
-	Dog( ) {}
-private:
-	std::string name = "";		// 10 이상 30 이하 임의의 소문자
-	int num = 0;
 
-	friend std::ostream& operator<<(std::ostream& os , const Dog& dog) {
-		std::print(os , "[{:4}] - {}" , dog.num , dog.name);
-		return os;
-	}
-};
+// [문제] 사용자가 원하는 개수 만큼 int를 저장할 메모리를 확보하라.
+// 메모리를 1부터 시작하는 int 값으로 채워라.
+// 합계를 계산하여 출력하라.
+// 정답
 
 int main( )
 {
-	// [문제] 바이너리 모드로 저장한 파일 "Dog백마리"에는 Dog 객체 100개가 저장되어 있다.
-	// Dog 객체는 파일의 write 함수를 사용하여 객체 크기 그대로 저장하였다.
+	std::cout << "원하는 만큼의 int 개수 입력: ";
+	int num;
+	std::cin >> num;
 
-	// 파일을 읽어 array에 저장하라.
-	// 객체 수 100개는 실행동안 변경되지 않는다.
+	// 문제없는 수만 입력할거라 희망
 
-	std::ifstream in{ "Dog백마리", std::ios::binary };
+	// int* p;	// 안 써야할 자료형
+	// C++11(Modern C++)부터 제대로 된 smart pointer가 있으니까
 
-	if ( not in ) {
-		std::cout << "파일을 열 수 없습니다" << std::endl;
-		return 2022184025;
-	}
+	std::unique_ptr<int[]> p{ new int[ num ] };		// RAII
 
-	std::array<Dog , 100> dogs;
-	in.read(reinterpret_cast< char* >( dogs.data( ) ) , sizeof(Dog) * dogs.size( ));
+	std::iota(p.get( ) , p.get( ) + num , 1);
 
-	for ( const Dog& dog : dogs ) {
-		std::cout << dog << std::endl;
-	}
+	long long sum = std::accumulate(p.get( ) , p.get( ) + num , 0LL);
+
+	std::cout << "1부터 " << num << "까지의 합계는 " << sum << std::endl;
 
 	save("main.cpp");		// "메인.cpp"를 저장하자
 }

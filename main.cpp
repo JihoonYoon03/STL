0fb48ed24ba822e9cfa6881ddccc53554c318869
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-03-24 23:26:38 +0900

    Add: 메모리 할당 예외 수정버전

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <numeric>
 #include <memory>
+#include <string>
 
 #include "save.h"
 
@@ -14,26 +15,45 @@
 // [문제] 사용자가 원하는 개수 만큼 int를 저장할 메모리를 확보하라.
 // 메모리를 1부터 시작하는 int 값으로 채워라.
 // 합계를 계산하여 출력하라.
-// 정답
+// 발생 가능한 오류 차단 버전
 
 int main( )
 {
 	std::cout << "원하는 만큼의 int 개수 입력: ";
-	int num;
-	std::cin >> num;
+	std::string input;
+	std::getline(std::cin, input);
 
-	// 문제없는 수만 입력할거라 희망
+	int num{ };
+
+	try {
+		size_t pos{};
+		num = std::stoi(input, &pos);
+
+		if ( pos != input.length( ) ) {
+			throw std::invalid_argument("문자가 포함됨");
+		}
+	}
+	catch (...) {
+		std::cout << "잘못된 입력 혹은 너무 큰 수 입니다." << std::endl;
+		exit(2022184025);
+	}
 
 	// int* p;	// 안 써야할 자료형
 	// C++11(Modern C++)부터 제대로 된 smart pointer가 있으니까
 
-	std::unique_ptr<int[]> p{ new int[ num ] };		// RAII
+	try {
+		std::unique_ptr<int[]> p{ new int[ num ] };		// RAII
 
-	std::iota(p.get( ) , p.get( ) + num , 1);
+		std::iota(p.get( ) , p.get( ) + num , 1);
 
-	long long sum = std::accumulate(p.get( ) , p.get( ) + num , 0LL);
+		long long sum = std::accumulate(p.get( ) , p.get( ) + num , 0LL);
 
-	std::cout << "1부터 " << num << "까지의 합계는 " << sum << std::endl;
+		std::cout << "1부터 " << num << "까지의 합계는 " << sum << std::endl;
+	}
+	catch (...) {
+		std::cout << "잘못된 개수입니다." << std::endl;
+		exit(2022184025);
+	}
 
 	save("main.cpp");		// "메인.cpp"를 저장하자
 }

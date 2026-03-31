3847675c47a9baa2e22e89a77ebac636113c5c03
laptop-msi <wlgns7922@gmail.com>
2026-03-31 15:18:33 +0900

    Add: callable type 공부

--- main.cpp
+++ main.cpp
@@ -1,53 +1,31 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				3월 25일					(4주 2일)
+// 2026 1학기 STL				3월 31일					(5주 1일)
 //------------------------------------------------------------------
-// callable type
+// 호출가능한 타입 - callable type
+// 1. 함수
+// 2. lambda - 이름 없는 함수
+// 3. 함수 호출 연산자를 오버로딩 한 클래스
+// 4. 멤버 함수 포인터
+// 
+// 통합하여 function 클래스로 사용
 //------------------------------------------------------------------
 
 #include <iostream>
-#include <random>
-#include <array>
-#include <print>
-#include <ranges>
 
 #include "save.h"
 
-// 랜덤값을 갖는 int 1000만개를 메모리에 저장하라.
-// 오름차순 정렬 후 출력하라
-
-std::default_random_engine		dre{};
-std::uniform_int_distribution	uid{ 0, 999'9999 };
-
-// a와 a{1} 했을 때 파일 크기 차이나는 이유
-std::array<int, 1000'0000> a;
-
-int ascending(const void* a , const void* b) {
-	// void* - 사용자가 마음대로 바꿀 수 있는 형식
-	// 정렬 함수는 정렬할 타입을 알지만, qsort는 void*로 넘겨주므로 인자가 이렇게 됨
-	return *( int* )a - *( int* )b;
-}
+class XYZ {
+public:
+	void operator()( ) {
+		std::cout << "안녕 난 람다야" << std::endl;
+	}
+};
 
 int main( )
 {
-	for ( int& num : a ) {
-		num = uid(dre);
-	}
+	XYZ a;
+	a( );
+	std::cout << typeid( a ).name( ) << std::endl;
 
-	std::cout << "정렬 전 - 앞에서부터 1000개만 출력" << std::endl;
-
-	for ( const int& num : a | std::views::take(1000) ) {	// | : 필터. 주어진 데이터를 원하는 방식으로 필터링. 중첩 가능함
-		std::print("{:8}" , num);
-	}
-
-	// 여기서 qsort를 사용하여 오름차순으로 정렬하라.
-	// 전달되는 인자 중 앞의 3개는 물리량, 마지막은 코드이다.
-	std::qsort(a.data( ) , a.size( ) , sizeof(std::array<int, 1000'0000>::value_type) , ascending);
-
-	std::cout << "정렬 후 - 앞에서부터 1000개만 출력" << std::endl;
-
-	for ( const int& num : a | std::views::reverse | std::views::take(1000) ) {	// | : 필터. 주어진 데이터를 원하는 방식으로 필터링. 중첩 가능함
-		std::print("{:8}" , num);
-	}
-
-	//save("main.cpp");		// "메인.cpp"를 저장하자
+	save("main.cpp");		// "메인.cpp"를 저장하자
 }

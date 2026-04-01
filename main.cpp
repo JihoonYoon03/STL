a9305e6d123cc53b7ab722110ddcccdd5744b974
laptop-msi <wlgns7922@gmail.com>
2026-04-01 17:13:48 +0900

    연습문제

--- main.cpp
+++ main.cpp
@@ -1,31 +1,52 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				3월 31일					(5주 1일)
+// 2026 1학기 STL				4월 1일					(5주 2일)
 //------------------------------------------------------------------
-// 호출가능한 타입 - callable type
-// 1. 함수
-// 2. lambda - 이름 없는 함수
-// 3. 함수 호출 연산자를 오버로딩 한 클래스
-// 4. 멤버 함수 포인터
-// 
-// 통합하여 function 클래스로 사용
+// 연습
 //------------------------------------------------------------------
 
 #include <iostream>
+#include <fstream>
+#include <array>
+#include <ranges>
+#include <print>
 
 #include "save.h"
 
-class XYZ {
-public:
-	void operator()( ) {
-		std::cout << "안녕 난 람다야" << std::endl;
+class Dog {
+	friend std::ostream& operator<<(std::ostream& os , const Dog& dog) {
+		std::print("{:7}, {}" , dog.id , dog.name);
+		return os;
 	}
+
+	Dog& operator=(Dog& other) {
+		*this = other;
+	}
+
+private:
+	std::string name;	// [1, 16]
+	size_t		id;		// [1, 999'9999]
 };
+
+// [문제] 바이너리 파일 "Dog천만마리"에는 write로 Dog 천만 객체를 기록하였다.
+// array에 모든 객체를 읽어와라.
+// 앞에서 100개를 화면에 출력하라
+
+std::array<Dog , 1000'0000> dogs;
 
 int main( )
 {
-	XYZ a;
-	a( );
-	std::cout << typeid( a ).name( ) << std::endl;
+	std::ifstream in{ "Dog천만마리", std::ios::binary };
 
-	save("main.cpp");		// "메인.cpp"를 저장하자
+	if ( not in ) {
+		std::cout << "파일 읽기 문제 발생" << std::endl;
+		return 2022184025;
+	}
+
+	std::copy(std::istreambuf_iterator(in), std::istreambuf_iterator(in), dogs.begin());
+
+	for ( const Dog& dog : dogs | std::views::take(100) ) {
+		std::cout << dog << std::endl;
+	}
+
+	//save("main.cpp");		// "메인.cpp"를 저장하자
 }

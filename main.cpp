edd5b92fab50b05df049c6f984ea91b353343c78
laptop-msi <wlgns7922@gmail.com>
2026-05-12 15:18:11 +0900

    deque, forward_list 개념과 span객체 개념

--- main.cpp
+++ main.cpp
@@ -1,18 +1,16 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				5월 6일					(10주 2일)
+// 2026 1학기 STL				5월 12일					(11주 1일)
 //------------------------------------------------------------------
-// STL Container - Containers are objects that store other objects.
-// Sequence Container
-// array<T, N>	- 유일하게 크기 고정된 자료구조 -> STACK, DATA
-// vector<T>	- 캐시 효율성 (Cache Locality)
-// list<T>		- 임의의 위치에서 추가/제거 동작이 O(1)
-// deque<T>		- vector와 list의 장점을 활용하려는 자료구조
+// span<T> - light weight object
+// contiguous 메모리를 대표(포장)함 (like std::function)
+// string의 경우 중요한 자료구조 - string_view라는 특별한 타입 존재
+// syntactic sugar 이다.
+// 
 //------------------------------------------------------------------
-
+ 
 #include <iostream>
-#include <vector>
-#include <deque>
-#include <list>
+#include <algorithm>
+#include <span>
 
 #include "save.h"
 #include "YString.h"
@@ -21,44 +19,17 @@ extern bool observe;
 
 int main( )
 {
+	YString ys{ "The quick brown fox jumps over the lazy dog" };
+
+	std::sort(ys.data( ) , ys.data( ) + ys.size( ));
+
+	// [문제] 거꾸로 출력하시오
+	std::span<char> s{ ys.data( ), ys.size( ) };
+	
+	for ( auto i = s.rbegin( ); i != s.rend( ); ++i ) {
+		std::cout << *i << " - ";
+	}
+	std::cout << std::endl;
+
 	save("main.cpp");		// "메인.cpp"를 저장하자
-	// vector, deque, list 어떤 것이 가장 많은 원소를 담을 수 있는가?
-	{
-		std::vector<int> v;
-		while ( true ) {
-			try {
-				v.push_back(1);
-			}
-			catch ( ... ) {
-				std::cout << "vector<int> 개수 - " << v.size() << std::endl;
-				break;
-			}
-		}
-	}
-	{
-		std::deque<int> v;
-		while ( true ) {
-			try {
-				v.push_back(1);
-			}
-			catch ( ... ) {
-				std::cout << "deque<int> 개수 - " << v.size( ) << std::endl;
-				break;
-			}
-		}
-	}
-	{
-		std::list<int> v;
-		while ( true ) {
-			try {
-				v.push_back(1);
-			}
-			catch ( ... ) {
-				std::cout << "list<int> 개수 - " << v.size( ) << std::endl;
-				break;
-			}
-		}
-	}
-
-
 }

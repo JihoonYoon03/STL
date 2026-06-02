43eb1713b45e416bcec7179034ba404b4dfcb0d3
laptop-msi <wlgns7922@gmail.com>
2026-06-02 15:18:28 +0900

    set, map 실습. 정규분포 실습. unordered_set/multiset 실습

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 //------------------------------------------------------------------
-// 2026 1학기 STL				5월 26일					(13주 1일)
+// 2026 1학기 STL				6월 2일					(13주 2일)
 // 기말시험 6/17
 //------------------------------------------------------------------
-// STL Associative Container
-// Key, Value - Key값을 정렬 기준으로 O(log N) 복잡도로 access
-// - set, multiset		K == V
-// - map, multimap
+// STL Unordered Associative Container
+// Key, Value - O(1) 복잡도로 key값에 access
+// - unordered_set, unordered_multiset		K == V
+// - unordered_map, unordered_multimap
 //------------------------------------------------------------------
 
 #include <iostream>
-#include <map>
-#include <fstream>
-#include <algorithm>
+#include <unordered_set>
+#include <print>
 
 #include "save.h"
 #include "YString.h"
@@ -22,39 +21,28 @@ int main( )
 {
 	//save("main.cpp");
 
-	std::ifstream in{ "이상한 나라의 앨리스.txt" };
-	if ( not in ) {
-		std::cout << "파일 오류";
-		return 20260527;
+	// [문제] unordered_multiset의 메모리 구조를 출력해본다.
+	// hash는 다음 시간에
+	std::unordered_multiset<int> ums;
+
+	while ( true ) {
+		std::cout << "추가할 int값 - ";
+		int num;
+		std::cin >> num;
+		std::cout << std::endl;
+
+		ums.insert(num);
+		std::cout << std::hash<int>( )( num ) << std::endl;
+
+		for ( int i = 0; i < ums.bucket_count( ); ++i ) {
+			std::print("[{:>4}]" , i);
+			// i번째 버킷 내부 순회
+			for ( auto p = ums.begin(i); p != ums.end(i); ++p ) {
+				std::cout << " <-> " << *p;
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
 	}
 
-	// [문제] 파일에 있는 단어 개수를 다음과 같이 출력하라
-	// a		- 
-	// aa		-
-	// ...
-	// zigzag	-
-
-	std::map<YString , size_t> ss;
-	YString ys;
-
-	while ( in >> ys ) {
-		++ss[ ys ];
-	}
-
-	for ( const auto& [word , cnt] : ss ) {
-		std::cout << word << " - " << cnt << std::endl;
-	}
-
-	std::cout << "단어 개수 - " << ss.size( ) << std::endl << std::endl;
-
-	// 많이 나온 단어 순서대로 출력하라
-	std::map<size_t , YString , std::greater<size_t>> sys;
-
-	for ( const auto& [word , cnt] : ss ) {
-		sys.insert(std::make_pair(cnt , word));
-	}
-
-	for ( const auto& [cnt , word] : sys ) {
-		std::cout << cnt << " - " << word << std::endl;
-	}
 }
